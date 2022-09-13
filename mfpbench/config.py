c4bfee7642a87c863427dd3c356c2d03cf322cab
eddiebergman <eddiebergmanhs@gmail.com>
2022-09-13 18:50:01 +0200

    Extend capabilities of configs to be priors, load from file

--- mfpbench/config.py
+++ mfpbench/config.py
@@ -3,9 +3,12 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Any, Mapping, TypeVar
 
+import json
 from dataclasses import asdict, dataclass
+from pathlib import Path
 
-from ConfigSpace import Configuration
+import yaml
+from ConfigSpace import Configuration, ConfigurationSpace
 
 # Just so `def copy(...)` can give back the correct type
 SelfT = TypeVar("SelfT", bound="Config")
@@ -63,3 +66,60 @@ class Config(ABC):
             return self.dict() == other.dict()
         else:
             return False
+
+    def set_as_default_prior(self, configspace: ConfigurationSpace) -> None:
+        """Applies this configuration as a prior on a configspace
+
+        Note
+        ----
+        If there is some renaming that needs to be done, this should be overwritten
+
+        Parameters
+        ----------
+        configspace: ConfigurationSpace
+            The space to apply this config to
+        """
+        for attr in iter(k for k in self.__annotations__ if not k.startswith("_")):
+            hp = configspace[attr]
+            value = getattr(self, attr)
+            hp.default_value = hp.check_default(value)
+
+    @classmethod
+    def from_file(cls: type[SelfT], path: Path) -> SelfT:
+        """Load a config from a supported file type
+
+        Note
+        ----
+        Only supports yaml and json for now
+        """
+        if path.suffix == "json":
+            return cls.from_json(path)
+        if path.suffix in ["yaml", "yml"]:
+            return cls.from_yaml(path)
+
+        # It has no file suffix, just try both
+        try:
+            return cls.from_yaml(path)
+        except yaml.error.YAMLError:
+            pass
+
+        try:
+            return cls.from_json(path)
+        except json.JSONDecodeError:
+            pass
+
+        raise ValueError(f"Path {path} is not valid yaml or json")
+
+    @classmethod
+    def from_yaml(cls: type[SelfT], path: Path) -> SelfT:
+        """Load a config from a yaml file"""
+        with path.open("r") as f:
+            d = yaml.safe_load(f)
+            return cls(**d)
+
+    @classmethod
+    def from_json(cls: type[SelfT], path: Path) -> SelfT:
+        """Load a config from a json file"""
+        with path.open("r") as f:
+            d = json.load(f)
+            return cls(**d)
